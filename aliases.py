7a3efe94e8897b63afafb07ed92a6f52eff33460
Shane <Shane@skysafe.io>
2018-04-18 08:30:10 -0700

    Cleaned up aliases.py

--- aliases.py
+++ aliases.py
@@ -1,24 +1,24 @@
+#!/usr/bin/python
 """
 Set up aliases for dotfiles and folders.
 """
-
+from __future__ import print_function
 import sys, os
 
 base = os.path.dirname(os.path.realpath(__file__))
 home = os.environ['HOME']
 
-print "Base Dir: {}".format(base)
-print "home Dir: {}".format(home)
+print("Base Dir: {}".format(base))
+print("home Dir: {}".format(home))
 
 for destfile in os.listdir(base):
-    print destfile
     if destfile.startswith('dot.'):
         srcfile = ".%s" % destfile[4:]
         dest = os.path.join(base, destfile)
         src = os.path.join(home, srcfile)
-        print("linking %s -> %s" % (src, dest))
+        print("[+] Linking {} -> {}".format(src, dest))
         try:
             os.symlink(dest, src)
         except OSError:
-            print("  skipping! file already exists")
+            print("[-] WARNING: Skipping! file already exists")
 
