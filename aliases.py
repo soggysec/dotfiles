a57754c20e49f46cfa6f51a07d91391af577efed
Shane <shane@skysafe.io>
2018-07-24 15:56:26 -0700

    Making it more user friendly

--- aliases.py
+++ aliases.py
@@ -4,6 +4,7 @@ Set up aliases for dotfiles and folders.
 """
 from __future__ import print_function
 import sys, os
+import shutil
 
 base = os.path.dirname(os.path.realpath(__file__))
 home = os.environ['HOME']
@@ -11,14 +12,29 @@ home = os.environ['HOME']
 print("Base Dir: {}".format(base))
 print("home Dir: {}".format(home))
 
-for destfile in os.listdir(base):
-    if destfile.startswith('dot.'):
-        srcfile = ".%s" % destfile[4:]
-        dest = os.path.join(base, destfile)
-        src = os.path.join(home, srcfile)
+for srcfile in os.listdir(base):
+    if srcfile.startswith('dot.'):
+        destfile = ".%s" % srcfile[4:]
+        src = os.path.join(base, srcfile)
+        dest = os.path.join(home, destfile)
         print("[+] Linking {} -> {}".format(src, dest))
+        overwrite = False
+        if os.path.exists(dest):
+            while True:
+                resp = raw_input("\t{} - already exists, would you like to overwrite? [Y/n]? ".format(dest))
+                if resp in ['', 'y','Y']:
+                    overwrite = True
+                    break
+                elif resp == "n":
+                    # skip - don't overwrite
+                    break
+
         try:
-            os.symlink(dest, src)
+            if overwrite:
+                backup = "{}.orig".format(dest)
+                shutil.move(dest, backup)
+                print("\tBacked up {} to {}".format(dest, backup))
+            os.symlink(src, dest)
         except OSError:
-            print("[-] WARNING: Skipping! file already exists")
+            print("[-] WARNING: File already exists, skipping: {}".format(dest))
 
