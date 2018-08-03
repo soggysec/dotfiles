3fef53ec7a91c96cf07b033cbbeec50e26f8f0a5
Shane Shellenbarger <shane@skysafe.io>
2018-08-02 17:03:20 -0700

    Adding more stuff for i3

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt install vim git python-dev ipython python-pip
+sudo apt install vim git python-dev ipython python-pip j4-dmenu-desktop scrot i3
 pip install virtualenvwrapper
 pip install pipenv
